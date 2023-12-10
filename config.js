67a8ab120d599edd443b5e4940030b60a128c8ce
云端之风 <1479221500@qq.com>
2023-12-11 02:18:35 +0800

    feat: V3.6

--- config.js
+++ config.js
@@ -10,11 +10,12 @@ module.exports = {
     database: "china_area",
     charset: "utf8mb4",
   },
-  TB: "area_10", // 表名,json文件,js文件名
+  TB: "area", // 表名,json文件,js文件名
   NO_ZX: true, // 有一些 "省直辖县级行政区划" 和 "自治区直辖县级行政区划" 如果是true则跳过该级别直接采集里面的数据
   SHOW_LINK: true, // 是否输出链接地址
   RE_TRY: 20, // 重试次数
   DELAY_MS: 5 * 1000, // 重试延迟时间, 会进行次数叠加
+  EXP_FIELD: ["id", "pid", "code", "name", "level"], // 导出保留字段
 
   /* -- 下列是备用的配置项，暂时没用到 -- */
   SHORT_CODE: false, // 短的code(如果采集的数据<=3级,并且此项为true,那么采集到的code会进行精简)
--- tools.js
+++ tools.js
@@ -5,7 +5,6 @@ const TB = conf.TB;
 const argv = process.argv;
 //console.log(argv)
 
-
 /* 
     node tools.js
     node tools.js json
@@ -14,53 +13,54 @@ const argv = process.argv;
 */
 
 let ext = argv[2] || "json", // 导出文件类型 json  js  txt
-    type = argv[3] || "sql", // 从哪里导出 sql  txt (txt后续版本开发)
-    exportFile = "./data/";
+  type = argv[3] || "sql", // 从哪里导出 sql  txt (txt后续版本开发)
+  exportFile = "./data/";
 
-let kenx
+let kenx;
 
 if (type == "sql") {
-    knex = require("knex")({
-        client: "mysql2",
-        connection: conf.DB,
-        pool: { min: 0, max: 7 },
-    });
+  knex = require("knex")({
+    client: "mysql2",
+    connection: conf.DB,
+    pool: { min: 0, max: 7 },
+  });
 }
 
 if (!["json", "js", "txt"].includes(ext)) {
-    ext = "json";
+  ext = "json";
 }
 exportFile += `${TB}.${ext}`;
 exp();
 
 // 生成文件
 async function exp() {
-    let hasTab = await knex.schema.hasTable(TB);
-    if (!hasTab) {
-        com.elog(`## 表不存在`);
-        knex.destroy();
-        return;
-    }
-    const sourceList = await knex.select().from(TB);
-    let list = com.generatTree2(sourceList);
-    switch (ext) {
-        case "js":
-            com.wFile(exportFile, `const areaList = ${JSON.stringify(list)}`);
-            break;
-        case "txt":
-            expTxt(sourceList);
-            break;
-        default: // json
-            com.wFile(exportFile, JSON.stringify(list));
-    }
+  let hasTab = await knex.schema.hasTable(TB);
+  if (!hasTab) {
+    com.elog(`## 表不存在`);
     knex.destroy();
+    return;
+  }
+  const sourceList = await knex.select(...conf.EXP_FIELD).from(TB);
+  let list = com.generatTree2(sourceList);
+  switch (ext) {
+    case "js":
+      com.wFile(exportFile, `const areaList = ${JSON.stringify(list)}`);
+      break;
+    case "txt":
+      expTxt(sourceList);
+      break;
+    default: // json
+      com.wFile(exportFile, JSON.stringify(list));
+  }
+  knex.destroy();
 }
 
-
 function expTxt(list) {
-    let str = `ID\t父级ID\t代码\t代码2\t名称\t级别`;
-    for (const row of list) {
-        str += `\r\n${row.id}\t${row.pid}\t${row.code}\t${row.code2}\t${row.name}\t${row.level}`
-    }
-    com.wFile(exportFile, str)
+  let str = `ID\t父级ID\t代码\t代码2\t名称\t级别`;
+  for (const row of list) {
+    str += `\r\n${row.id}\t${row.pid}\t${row.code}\t${row.code2 || ""}\t${
+      row.name
+    }\t${row.level}`;
+  }
+  com.wFile(exportFile, str);
 }
